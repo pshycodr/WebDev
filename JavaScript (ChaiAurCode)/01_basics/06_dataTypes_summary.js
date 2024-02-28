77d0e0c767241d1015ced0932fda235ae0735be0
pshycodr <115141578+pshycodr@users.noreply.github.com>
2024-02-28 13:35:04 +0530

    .

---
+++ JavaScript (ChaiAurCode)/01_basics/06_dataTypes_summary.js
@@ -0,0 +1,25 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100; // Number
+const scoreValue = 100.3;
+
+const str = "Hello World" //String
+
+const IsLoggedin = false; //Boolean
+const outsideTemp = null; // Null
+let useEmail; // undefined
+
+const id = symbol('123') // symbol
+const anotherId = symbol('123')
+
+const bigInt = 73837363524234364647n // BigInt
+
+
+
+
+// Refrence Type or Non Primitive
+
+// Array, Objects, Functions
+
---
+++ JavaScript (ChaiAurCode)/02_basics/try.js
@@ -0,0 +1,18 @@
+function one(){
+    console.log("one");
+    two()
+}
+
+function two(){
+    console.log("two")
+    three()
+}
+
+function three(){
+    console.log("three")
+}
+
+
+one();
+two();
+three();
