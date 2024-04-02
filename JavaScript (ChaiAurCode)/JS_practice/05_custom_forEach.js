187ca92a1d9811614ac15de3600e0daf79e67c70
pshycodr <115141578+pshycodr@users.noreply.github.com>
2024-04-02 10:26:06 +0530

    .

--- JavaScript (ChaiAurCode)/JS_practice/05_custom_forEach.js
+++ JavaScript (ChaiAurCode)/JS_practice/05_custom_forEach.js
@@ -15,29 +15,29 @@
 /* Make a custom ForEach */
 
 
-// Array.prototype.ForEvery = function(callback, CurrentContext) {
-//     if(typeof callback !== 'function'){
-//         throw `${callback} is not a function`
-//     }
+Array.prototype.ForEvery = function(callback, CurrentContext) {
+    if(typeof callback !== 'function'){
+        throw `${callback} is not a function`
+    }
 
-//     let length = this.length;
+    let length = this.length;
 
-//     let i = 0;
+    let i = 0;
 
-//     while(i<length){
-//         if(this.hasOwnProperty(i)){
-//             callback.call(CurrentContext, this[i], i, this)
-//         }
-//         i++
-//     }
-// }
+    while(i<length){
+        if(this.hasOwnProperty(i)){
+            callback.call(CurrentContext, this[i], i, this)
+        }
+        i++
+    }
+}
 
 
-// const myArray = [1,2,3,4,5,4,3,4,8]
+const myArray = [1,2,3,4,5,4,3,4,8]
 
-// myArray.ForEvery((item) => {
-//     console.log(item)
-// })
+myArray.ForEvery((item) => {
+    console.log(item)
+})
 
 
 
