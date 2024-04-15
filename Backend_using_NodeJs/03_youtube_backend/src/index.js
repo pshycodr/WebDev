edda3a967a7270b8fb8255f6ea36530e218fc336
pshycodr <115141578+pshycodr@users.noreply.github.com>
2024-04-16 01:56:49 +0530

    Mongo DB connection successfull

---
+++ Backend_using_NodeJs/03_youtube_backend/src/index.js
@@ -0,0 +1,46 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./env",
+});
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* THIS APPROCH IS CORRECT BUT NOT SO GOOD ENOUGH
+
+import express from "express";
+const app = express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listing in ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw error;
+    }
+})();
+*/
